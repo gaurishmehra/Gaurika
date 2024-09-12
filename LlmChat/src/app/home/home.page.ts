918dfe5deb8ed6e96d6b0e4ab1fd7675d944e3e1
Gaurish Mehra <gaurishyt@gmail.com>
2024-09-12 23:30:00 +0530

    smol ui change uwu

--- LlmChat/src/app/home/home.page.ts
+++ LlmChat/src/app/home/home.page.ts
@@ -75,7 +75,7 @@ export class HomePage implements OnInit {
 
     if (this.platform.is('android')) {
       StatusBar.setStyle({ style: Style.Dark });
-      StatusBar.setBackgroundColor({ color: '#1e1e1e' }); 
+      StatusBar.setBackgroundColor({ color: '#0a0a0a' }); 
     }
   }
 
