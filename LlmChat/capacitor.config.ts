677aa5fcb37ca2c06b918d7bde58d5901ca63770
Gaurish Mehra <gaurishyt@gmail.com>
2024-09-12 19:43:42 +0530

    234

--- LlmChat/capacitor.config.ts
+++ LlmChat/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'llmchat.gaurish.xyz',
   appName: 'LlmChat',
   webDir: 'www'
 };
