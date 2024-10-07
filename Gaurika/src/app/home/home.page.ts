8504ab8eaafbe9044eb9a340bf9ad2f3510daca6
Gaurish Mehra <gaurishyt@gmail.com>
2024-10-07 23:37:56 +0530

    update background and input colors for better contrast

--- Gaurika/src/app/home/home.page.ts
+++ Gaurika/src/app/home/home.page.ts
@@ -142,7 +142,7 @@ export class HomePage implements OnInit {
 
     if (this.platform.is('android')) {
       StatusBar.setStyle({ style: Style.Dark });
-      StatusBar.setBackgroundColor({ color: '#0a0a0a' });
+      StatusBar.setBackgroundColor({ color: '#181818' });
     }
 
     const storedApiKey = await this.storage.get('apiKey');
